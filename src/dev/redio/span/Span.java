d4ec9de8352b74c2230f06dcd366cc67c4bcc2af
Luca Lehmann <redio.development@gmail.com>
2022-06-15 09:40:44 +0200

    base info

--- src/dev/redio/span/Span.java
+++ src/dev/redio/span/Span.java
@@ -1,9 +1,21 @@
 package dev.redio.span;
 
+import dev.redio.pointer.Keywords;
+import dev.redio.pointer.Pointer;
+import dev.redio.pointer.Utils;
+
 /**
  * Span
  */
 public class Span<T> {
+    private final Pointer<T> ptr;
+    private final int baseOffset;
+    private final int maxLength;
+    private int start;
+    private int length;
     
+    public Span(T[] array) {
+        
+    }
     
 }
